ba26c915a3eda0c1ebfeb207fa75883a401d5347
ninja <nithin3dev@gmail.com>
2025-11-27 21:40:37 +0530

    init

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,64 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
+import RNFS from 'react-native-fs';
+import ManageExternalStorage from 'react-native-external-storage-permission';
+
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { useEffect } from 'react';
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+    const reqPer = async () => {
+        try {
+            const granted = await ManageExternalStorage.checkAndGrantPermission();
+            if (granted) {
+                getFiles();
+            } else {
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    useEffect(() => {
+        reqPer()
+    }, []);
+    const getFiles = async () => {
+        const rootDir = RNFS.ExternalStorageDirectoryPath;
+        let files = [];
+        let queue = [rootDir];
+        while (queue.length > 0) {
+            const currentDir: string = queue.shift() || '';
+            const items = await RNFS.readDir(`${currentDir}`);
+            for (const item of items) {
+                if (item.isFile() && item.name.match(/\.(pdf)$/i)) {
+                    files.push({ path: item.path, name: item.name });
+                } else if (item.isDirectory()) {
+                    if (currentDir.includes('/Android')) continue;
+                    if (currentDir.includes('/.')) continue;
+                    queue.push(item.path);
+                }
+            }
+        }
+        files.length || files.push({ path: "NA", name: "empty" })
+        // setfile(files)
+
+    };
+    return (
+        <ThemedView>
+            <ThemedView style={styles.eventArea} darkColor="#151718">
+                <ThemedText style={{ flex: 0.8 }} type="title">List of File</ThemedText>
+                <TouchableOpacity onPress={() => { }} style={{ flex: 0.1 }}><ThemedText>theme</ThemedText></TouchableOpacity>
+            </ThemedView>
+        </ThemedView>
+    );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+    root: { flex: 1, position: "relative", },
+    eventArea: { flexDirection: 'row', position: 'relative', padding: 15, paddingTop: 25, justifyContent: "space-between", alignItems: 'center', },
+    loadSceen: { flex: 1, justifyContent: "center", alignItems: 'center', },
+    lisTxt: { alignSelf: 'flex-start', justifyContent: "center", padding: 7, borderRadius: 15, borderWidth: 1, width: 'auto', },
+    allert: { margin: 20, borderRadius: 12, padding: 20, elevation: 5, },
+    buttonViw: { flexDirection: 'row', justifyContent: 'space-between' }
 });
